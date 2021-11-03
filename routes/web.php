ef7595e84c260372f8dd3bb01fa4cb5ecc525729
Ramdani Kurnia <adiatma85@gmail.com>
2021-11-03 23:26:18 +0700

    basic admin panel fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\PermissionsController as AdminPermissionController;
+use App\Http\Controllers\Admin\RolesController as AdminRolesController;
+use App\Http\Controllers\Admin\UsersController as AdminUsersController;
+use App\Http\Controllers\Auth\ChangePasswordController as AuthChangePasswordController;
+
+// Testing Siam Auth Controller
 use App\Http\Controllers\SiamAuthController;
 
 /*
@@ -15,9 +23,54 @@ use App\Http\Controllers\SiamAuthController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/login');
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
 });
 
+Auth::routes(['register' => false]);
+
+// Admin Route
+Route::prefix('admin')
+    ->as('admin.')
+    ->middleware(['auth'])
+    ->group(function () {
+        // Home
+        Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+
+        // Permissions
+        Route::delete('permissions/destroy', [AdminPermissionController::class, 'massDestroy'])->name('permissions.massDestroy');
+        Route::resource('permissions', AdminPermissionController::class);
+
+        // Roles
+        Route::delete('roles/destroy', [AdminRolesController::class, 'massDestroy'])->name('roles.massDestroy');
+        Route::resource('roles', AdminRolesController::class);
+
+        // Users
+        Route::delete('users/destroy', [AdminUsersController::class, 'massDestroy'])->name('users.massDestroy');
+        Route::resource('users', AdminUsersController::class);
+    });
+
+Route::prefix('profile')
+    ->as('profile.')
+    ->middleware(['auth'])
+    ->group(function () {
+        if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+            Route::get('password', [AuthChangePasswordController::class, 'edit'])->name('password.edit');
+            Route::post('password', [AuthChangePasswordController::class, 'update'])->name('password.update');
+            Route::post('profile', [AuthChangePasswordController::class, 'updateProfile'])->name('password.updateProfile');
+            Route::post('profile/destroy', [AuthChangePasswordController::class, 'destroyProfile'])->name('password.destroyProfile');
+        }
+    });
+
+
+
+
+
+// Testing Auth Siam Controller
 // Route::post('/testing-siam-auth', [SiamAuthController::class, 'auth']);
 Route::get('/testing-siam-auth', [SiamAuthController::class, 'auth']);
