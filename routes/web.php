43d8025979e0b8dee9cd241c789aac329f5f543e
Ramdani Kurnia <adiatma85@gmail.com>
2021-11-04 20:57:33 +0700

    refix redirection

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,15 @@ use App\Http\Controllers\SiamAuthController;
 */
 
 Auth::routes(['register' => false]);
-Route::redirect('/', '/login');
+Route::redirect('/', 'pemilwa-fk/login');
+
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
 
 
 // Admin Route
